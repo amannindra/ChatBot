f24a150d4c2c62ee4b3d0667ea6b5d0eb8be438d
Aman Nindra <amannindra@gmail.com>
2024-09-11 14:31:37 -0700

    push

--- src/Main/chat/chat.jsx
+++ src/Main/chat/chat.jsx
@@ -1,25 +1,45 @@
 import "./chat.css";
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 function Chat() {
   const [userText, setuserText] = useState("");
-  const [userlist, setuserlist] = useState([]);
-
-
+  const [userlist, setuserlist] = useState([
+    { AI: "Hi User" },
+    { User: "Hi AI" },
+  ]);
   const handleSend = () => {
     alert(userText);
+    setuserlist((list) => [...{ User: userText }]);
     setuserText("");
+    fetch("http://127.0.0.1:5000/datas");
   };
+
+  useEffect(() => {
+    
+
+  }, [userlist]);
 
   return (
     <div className="center">
       <div className="chat">
-
-      
-        <div className="AI">Hello User</div>
-        <div className="User">Hello AI</div>
-        {/* messages will be rendered here */}
+        {userlist.map((entry, index) => {
+          if (entry.AI) {
+            return (
+              <div className="AI" key={index}>
+                {entry.AI}
+              </div>
+            );
+          } else if (entry.User) {
+            return (
+              <div className="User" key={index}>
+                {entry.User}
+              </div>
+            );
+          }
+        })}
       </div>
+
       <div className="user_input">
         <input
           placeholder="input"
